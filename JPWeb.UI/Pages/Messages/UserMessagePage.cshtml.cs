eb25bd604b670f393d7d91fc607e2060b2ab3294
Bradley Bergstrom <35245874+BradBergstrom@users.noreply.github.com>
2019-05-10 19:28:24 -0700

    Fixed Email implementation in admin messages
    Email/ messages is completely finihsed

--- JPWeb.UI/Pages/Messages/UserMessagePage.cshtml.cs
+++ JPWeb.UI/Pages/Messages/UserMessagePage.cshtml.cs
@@ -65,7 +65,7 @@ namespace JPWeb.UI.Pages.Messages
             };
 
             //at the moment the users name is set as their email
-            MailMessage message = new MailMessage("OregonTech3DPrintClub@donotreply.com", User.Identity.Name, "3D Print Club", newMsg.Body.ToString())
+            MailMessage message = new MailMessage("OregonTech3DPrintClub@donotreply.com", "THEPRE.S.Q.L@gmail.com", "3D Print Club", newMsg.Body.ToString())
             {
                 BodyEncoding = Encoding.UTF8,
                 DeliveryNotificationOptions = DeliveryNotificationOptions.OnFailure
