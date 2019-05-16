5d47db38885120bb9d17f4eac3849aa5ca472750
Bradley Bergstrom <35245874+BradBergstrom@users.noreply.github.com>
2019-05-16 16:04:30 -0700

    Added a Schedual Page
    V1 working

--- JPWeb.UI/Migrations/20190516192242_shedual.Designer.cs
+++ JPWeb.UI/Migrations/20190516192242_shedual.Designer.cs
@@ -4,14 +4,16 @@ using JPWeb.UI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace JPWeb.UI.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190516192242_shedual")]
+    partial class shedual
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ JPWeb.UI/Migrations/20190516192242_shedual.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace JPWeb.UI.Migrations
+{
+    public partial class shedual : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
