7056a146c61551477e2b75d447fc9f75590447a5
Andrew Doser <38057761+LoserAim@users.noreply.github.com>
2019-05-16 14:56:55 -0700

    Finished?

---
+++ JPWeb.UI/Controllers/AccountController.cs
@@ -0,0 +1,78 @@
+﻿using JPWeb.UI.Data.Model;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+
+
+namespace JPWeb.UI.Controllers
+{
+    [System.Web.Http.Authorize]
+    [RoutePrefix("api/Account")]
+    public class AccountController : ApiController
+    {
+
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        // POST api/Account/Register
+        [System.Web.Http.AllowAnonymous]
+        [System.Web.Http.Route("Register")]
+        public async Task<IHttpActionResult> Register(RegisterBindingModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var user = new ApplicationUser() { UserName = model.Email, Email = model.Email };
+
+            IdentityResult result = await _userManager.CreateAsync(user, model.Password);
+
+            if (!result.Succeeded)
+            {
+                return GetErrorResult(result);
+            }
+
+            return Ok();
+        }
+        private IHttpActionResult GetErrorResult(IdentityResult result)
+        {
+            if (result == null)
+            {
+                return InternalServerError();
+            }
+
+            if (!result.Succeeded)
+            {
+                if (result.Errors != null)
+                {
+                    foreach(var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.ToString());
+                    }
+                }
+
+                if (ModelState.IsValid)
+                {
+                    // No ModelState errors are available to send, so just return an empty BadRequest.
+                    return BadRequest();
+                }
+
+                return BadRequest(ModelState);
+            }
+
+            return null;
+        }
+    }
+}
