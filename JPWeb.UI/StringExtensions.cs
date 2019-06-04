8cbb4c5753a7d1b7b1686085b30fc6c109e41a4f
VorpW <wasseem.salame@oit.edu>
2019-06-04 10:27:25 -0700

    Fixed ProjectDescription and ProjectFile lengths

---
+++ JPWeb.UI/StringExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace JPWeb.UI
+{
+    public static class StringExtensions
+    {
+        public static string TruncateStringAndAddEllipses(this string @string, int maximumLength)
+        {
+            return @string.Length > maximumLength ? @string.Substring(0, maximumLength) + "..." : @string;            
+        }
+    }
+}
